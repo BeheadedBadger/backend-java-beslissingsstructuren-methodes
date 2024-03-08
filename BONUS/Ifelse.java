078cb5abd28c24b6f09879ed46ef8d8b3e360a8c
BeheadedBadger <ZarradeRuijter@gmail.com>
2024-03-08 12:58:19 +0100

    Opdracht1
    Poging tot het pushen van deze branch.

--- BONUS/Ifelse.java
+++ BONUS/Ifelse.java
@@ -1,7 +1,158 @@
 package BONUS;
 
 public class Ifelse {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        EvenOddChecker(0);
+        System.out.println(Grader(13));
+        System.out.println(PickLargestNumber(2,4));
+        positiveNegativeZeroChecker(0);
+        ageChecker("Atl", 16);
+        LeapYearChecker(1984);
+    }
+
+    //1. Een methode dat als input een getal van de gebruiker accepteert en controleert of het even of oneven is en dit print.
+    private static void EvenOddChecker(int number) {
+        if (number % 2 == 0) {
+            System.out.println(number + " is even");
+        } else {
+            System.out.println(number + " is odd");
+        }
+    }
+
+    //2. Een methode dat een cijfer van een student accepteert (tussen 1 en 10)
+    // en op basis van dat cijfer een bijbehorende letterwaarde toekent (A, B, C, D, F).
+    private static String Grader(int numbericalGrade) {
+        String Grade;
+        if (numbericalGrade == 1) {
+            Grade = "F-";
+        } else if (numbericalGrade == 2) {
+            Grade = "F";
+        } else if (numbericalGrade == 3) {
+            Grade = "D";
+        } else if (numbericalGrade == 4) {
+            Grade = "D+";
+        } else if (numbericalGrade == 5) {
+            Grade = "C";
+        } else if (numbericalGrade == 6) {
+            Grade = "C+";
+        } else if (numbericalGrade == 7) {
+            Grade = "B";
+        } else if (numbericalGrade == 8) {
+            Grade = "B+";
+        } else if (numbericalGrade == 9) {
+            Grade = "A";
+        } else if (numbericalGrade == 10) {
+            Grade = "A++! Great job!";
+        } else {
+            Grade = "Please enter a valid number.";
+        }
+
+        return Grade;
+    }
+
+    //3. Een programma dat de grootste van twee ingevoerde getallen bepaalt.
+    private static String PickLargestNumber(int numberA, int numberB)
+    {
+        if (numberA > numberB)
+        {
+            return numberA + " is larger than " + numberB;
+        }
+        else if (numberB > numberA)
+        {
+            return numberB + " is larger than " + numberA;
+        }
+        else if (numberA == numberB)
+        {
+            return numberA + " is equal to " + numberB;
+        }
+        else
+        {
+            return "Something went wrong! Try again!";
+        }
+    }
+
+    //4. Positief/Negatief/Zero Checker
+    private static void positiveNegativeZeroChecker(int number)
+    {
+        if (number > 0)
+        {
+            System.out.println("The number " + number + " is positive");
+        }
+        else if (number < 0 )
+        {
+            System.out.println("The number " + number + " is negative");
+        }
+        else if (number == 0)
+        {
+            System.out.println("The number " + number + " is zero, which isn't negative or positive!");
+        }
+    }
+
+    //5. Een methode dat de leeftijd van een persoon accepteert en bepaalt of die persoon een kind, puber, volwassene of senior is.
+    private static void ageChecker(String name, int age)
+    {
+        if (age < 2)
+        {
+            System.out.println(name + " is a little baby");
+        }
+
+        else if (age < 12)
+        {
+            System.out.println(name + " is a child");
+        }
+
+        else if (age < 21)
+        {
+            System.out.println(name + " is pubescent");
+        }
+
+        else if (age < 65)
+        {
+            System.out.println(name + " is an adult");
+        }
+
+        else
+        {
+            System.out.println(name + " is a senior");
+        }
+    }
+
+    //6. Tekstlengte Checker
+    private static void TextLengthCheck(String text)
+    {
+        if (text.length() < 10)
+        {
+            System.out.println("This text is short.");
+        }
+
+        else if (text.length() < 20)
+        {
+            System.out.println("This text is medium length.");
+        }
+
+        else if (text.length() > 20)
+        {
+            System.out.println("TL;DR");
+        }
+    }
+
+    //7: Schrikkeljaar?
+    private static void LeapYearChecker(int year)
+    {
+        if(year % 4 == 0 && year % 100 != 0)
+        {
+            System.out.println("The year " + year + " is a leap year");
+        }
+
+        else if(year % 400 == 0)
+        {
+            System.out.println("The year " + year + " is a leap year");
+        }
+
+        else
+        {
+            System.out.println("The year " + year + " is NOT a leap year");
+        }
     }
 }
--- BONUS/Loops.java
+++ BONUS/Loops.java
@@ -1,7 +1,144 @@
 package BONUS;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Loops {
     public static void main(String[] args) {
+        sumOfAllEvenNumbers();
+        powerOfTwo();
+        findNR45();
+        sumOfAllInts();
+        PrintAllStringsInList();
+        printAllLargerThan45();
+        countDownFrom10();
+        Fibonacci();
+        Triangle();
+    }
+
+    //1. Je moet de som van alle even getallen berekenen van 1 tot 100.
+    private static void sumOfAllEvenNumbers() {
+        List<Integer> numbers1to100 = new ArrayList<Integer>();
+
+        //Make the list
+        int i = 1;
+        while (i < 101) {
+            System.out.println("Add number to array: " + i);
+            numbers1to100.add(i);
+            i++;
+        }
+
+        //Add up all even numbers
+        int Sum = 0;
+        for (int number : numbers1to100) {
+            if (number % 2 == 0) {
+                Sum = Sum + number;
+            }
+        }
+
+        //Print to console
+        System.out.println("The sum of all the even numbers is: " + Sum);
+    }
+
+    //2. Je wilt een reeks van getallen afdrukken, beginnend bij 1 en elke keer vermenigvuldigd met 2, totdat het resultaat groter is dan 1000.
+    private static void powerOfTwo() {
+        int i = 1;
+        while (i < 1000) {
+            System.out.println(i);
+            i = i * 2;
+        }
+    }
+
+    //3. Je moet een lijst van getallen doorlopen en controleren of het getal 45 aanwezig is.
+    private static void findNR45() {
+        List<Integer> listOfNumbers = new ArrayList<>(List.of(17, 92, 3, 89, 55, 86, 24, 34, 67, 76, 81, 50, 59, 30, 14, 33, 83, 87, 38, 70, 9, 78, 91, 46, 53, 64, 25, 69, 29, 84, 22, 16, 79, 40, 21, 18, 68, 75, 2, 26, 39, 8, 31, 35, 1, 63, 77, 95, 15, 58, 61, 10, 85, 93, 96, 72, 98, 5, 73, 6, 23, 56, 49, 99, 43, 41, 20, 11, 12, 74, 57, 66, 94, 36, 7, 60, 88, 4, 65, 27, 28, 82, 32, 97, 48, 62, 80, 37, 13, 54, 19, 71, 42, 44, 90, 52, 51));
+
+        for (int number : listOfNumbers) {
+            if (number == 45) {
+                System.out.println("Number 45 found!");
+                return;
+            }
+        }
+
+        System.out.println("Number 45 not found");
+    }
+
+    //4. Je wilt een array van integers doorlopen en de som van de elementen berekenen.
+    private static void sumOfAllInts() {
+        List<Integer> listOfNumbers = new ArrayList<>(List.of(12, 5, 7, 2, 18, 3, 9, 12));
+        int Sum = 0;
+
+        for (int number : listOfNumbers) {
+            Sum = Sum + number; //Ik weet dat ik ook Sum += number kan doen, maar vind dit makkelijker te lezen :p Vergeef me.
+        }
+
+        System.out.println("The sum of all the numbers in the list: " + Sum);
+    }
+
+    //5. Je hebt een lijst van strings en je wilt elk element afdrukken totdat je een lege string tegenkomt.
+    private static void PrintAllStringsInList() {
+        List<String> listOfStrings = new ArrayList<>(List.of("Er", "was", "eens", "een", "student", "die", "graag", "wilde", "leren", "", "Deze", "student", "haalde", "goede", "cijfers"));
+
+        for (String word : listOfStrings) {
+            if (word == null || word.isEmpty()) {
+                return;
+            } else {
+                System.out.println(word);
+            }
+        }
 
     }
+
+    //7. Je hebt een lijst van integers en je wilt elk element afdrukken dat groter is dan 45.
+    private static void printAllLargerThan45() {
+        List<Integer> listOfNumbers = new ArrayList<>(List.of(17, 92, 3, 89, 55, 86, 24, 34, 67, 76, 81, 50, 59, 30, 14, 33, 83, 87, 38, 70, 9, 78, 91, 46, 53, 64, 25, 69, 29, 84, 22, 16, 79, 40, 21, 18, 68, 75, 2, 26, 39, 8, 31, 35, 1, 63, 77, 95, 15, 58, 61, 10, 85, 93, 96, 72, 98, 5, 73, 6, 23, 56, 49, 99, 43, 41, 20, 11, 12, 74, 57, 66, 94, 36, 7, 60, 88, 4, 65, 27, 28, 82, 32, 97, 48, 62, 80, 37, 13, 54, 19, 71, 42, 44, 90, 52, 51));
+
+        for (int number : listOfNumbers) {
+            if (number > 45) {
+                System.out.println(number);
+            }
+        }
+    }
+
+    //8. Je wilt een teller implementeren die begint bij 10 en wordt verminderd met 1 totdat het gelijk is aan 0.
+    private static void countDownFrom10() {
+        int i = 10;
+        while (i >= 0) {
+            System.out.println(i);
+            i--;
+        }
+    }
+
+    //9. Je moet de eerste 10 Fibonacci-getallen genereren. (tip: google is je vriend)
+    private static void Fibonacci() {
+        int PreviousNr1 = 0;
+        int PreviousNr2 = 1;
+        int FibonacciNr = 0;
+
+        int i = 0;
+        while (i <= 10) {
+            FibonacciNr = PreviousNr1 + FibonacciNr;
+            PreviousNr1 = PreviousNr2;
+            PreviousNr2 = FibonacciNr;
+
+            System.out.println(FibonacciNr);
+            i++;
+        }
+    }
+
+    //10. Je wilt een driehoek afdrukken met behulp van sterretjes, waarbij het aantal sterretjes in elke rij toeneemt. (tip: google is je vriend)
+    private static void Triangle() {
+        String blankSpace = "            ";
+        String stars = "*";
+        int rows = 1;
+
+        while (rows < 10) {
+            System.out.println(blankSpace + stars);
+
+            stars += "**";
+            blankSpace = blankSpace.substring(0, blankSpace.length() - 1);
+
+            rows++;
+        }
+    }
 }
+
